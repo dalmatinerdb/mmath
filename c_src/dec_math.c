29c6fc44fda649b07d9cf23c74742220256af422
Tomasz Szarstuk <szarsti@gmail.com>
2015-11-09 18:18:46 +0000

    Bump up patch version number

--- c_src/dec_math.c
+++ c_src/dec_math.c
@@ -1,6 +1,36 @@
 #include "erl_nif.h"
 #include "mmath.h"
 
+/*
+ 
+  Fast 64bit integer log10
+  WARNING: calling ilog10c(0) yields undefined behaviour!
+
+  THANKS TO COURTESY OF CAFXX, https://gist.github.com/CAFxX/ad150f2403a0604e14cc
+*/
+static int
+log10ll(long long v) {
+  static const uint64_t thr[64] = {
+    10000000000000000000ULL, 0, 0, 0, 1000000000000000000ULL, 0, 0, 100000000000000000ULL, 0, 0,
+       10000000000000000ULL, 0, 0, 0,    1000000000000000ULL, 0, 0,    100000000000000ULL, 0, 0,
+          10000000000000ULL, 0, 0, 0,       1000000000000ULL, 0, 0,       100000000000ULL, 0, 0,
+             10000000000ULL, 0, 0, 0,          1000000000ULL, 0, 0,          100000000ULL, 0, 0,
+                10000000ULL, 0, 0, 0,             1000000ULL, 0, 0,             100000ULL, 0, 0,
+                   10000ULL, 0, 0, 0,                1000ULL, 0, 0,                100ULL, 0, 0,
+                      10ULL, 0, 0, 0
+  };
+  uint32_t lz = __builtin_clzll(v);
+  return (63 - lz) * 3 / 10 + (v >= thr[lz]);
+}
+
+/* Fast 64bit integer exp */
+static int
+exp10i() {
+  static const uint64_t thr[64] = {
+  };
+}
+
+
 
 decimal
 dec_mul(decimal v, long long m) {
