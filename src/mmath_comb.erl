5815ca100928a4edc489311222e105997db7b87d
Heinz N. Gies <heinz@licenser.net>
2016-04-29 22:21:15 +0200

    Document mmath_comb

--- src/mmath_comb.erl
+++ src/mmath_comb.erl
@@ -41,6 +41,12 @@ load_nif() ->
              end,
     erlang:load_nif(SoName, 0).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Creates a new dataset with each element being the sum of the
+%% elements of the passed datasets.
+%% @end
+%%--------------------------------------------------------------------
 sum([A, B]) ->
     sum(A, B);
 
@@ -51,80 +57,33 @@ sum(Es) ->
     rcomb(fun sum/2, fun sum/3, Es, []).
 
 
+
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Creates a new dataset with each element being the average (mean)
+%% of the elements of the passed datasets.
+%% @end
+%%--------------------------------------------------------------------
+avg(Es) ->
+    mmath_trans:divide(sum(Es), length(Es)).
+
+%%-------------------------------------------------------------------
+%% Utility functions
+%%-------------------------------------------------------------------
+
 sum(_A, _B) ->
     exit(nif_library_not_loaded).
 
 sum(_A, _B, _C) ->
     exit(nif_library_not_loaded).
 
-
-avg(Es) ->
-    mmath_trans:divide(sum(Es), length(Es)).
-
-%%merge(Es) ->
-%%    rcomb(fun merge/2, Es, []).
-
-
-%% mul(Es) ->
-%%     rcomb(fun mul/2, Es, []).
-
-%% mul(A, B) ->
-%%     zip(fun (VA, VB) ->
-%%                 VA * VB
-%%         end, A, B).
-
-%% zip(Fn, Es) ->
-%%     rcomb(fun (A, B) -> zip(Fn, A, B) end, Es, []).
-
-%% zip(Fn, A, B) ->
-%%     zip(A, B, 1, 1, Fn, <<>>).
-
-%% zip(<<>>, <<>>, _LA, _LB, _Fn, Acc) ->
-%%     Acc;
-
-%% zip(<<DA:?DATA_SIZE/binary, RA/binary>>,
-%%     <<DB:?DATA_SIZE/binary, RB/binary>>, LA, LB, Fn, Acc) ->
-%%     A = binary_to_value(DA, LA),
-%%     B = binary_to_value(DB, LB),
-%%     zip(RA, RB, A, B, Fn, apply(Fn, A, B, Acc)).
-
-%% binary_to_value(<<?NONE:?TYPE_SIZE, _:?BITS/?INT_TYPE>>, L) ->
-%%     L;
-%% binary_to_value(<<?INT:?TYPE_SIZE, I:?BITS/?INT_TYPE>>, _L) ->
-%%     I;
-%% binary_to_value(<<D:?DATA_SIZE/binary>>, _L) ->
-%%     [V] = mmath_bin:to_list(D),
-%%     V.
-
-%% apply(Fn, A, B, Acc) ->
-%%     case Fn(A, B) of
-%%         V when is_integer(V), abs(V) < (1 bsl (?BITS - 1)) ->
-%%             <<Acc/binary, ?INT:?TYPE_SIZE, (V):?BITS/?INT_TYPE>>;
-%%         V ->
-%%             <<Acc/binary, (mmath_bin:from_list([float(V)]))/binary>>
-%%     end.
-
-%% merge(A, B) ->
-%%     merge(A, B, <<>>).
-
-%% merge(<<?NONE:?TYPE_SIZE, _:?BITS/?INT_TYPE, R1/binary>>,
-%%       <<D:?DATA_SIZE/binary, R2/binary>>,
-%%       Acc) ->
-%%     merge(R1, R2, <<Acc/binary, D/binary>>);
-%% merge(<<D:?DATA_SIZE/binary, R1/binary>>,
-%%       <<_:?DATA_SIZE/binary, R2/binary>>,
-%%       Acc) ->
-%%     merge(R1, R2, <<Acc/binary, D/binary>>);
-%% merge(<<>>, <<>>, Acc) ->
-%%     Acc;
-
-%% merge(<<>>, D, Acc) ->
-%%     <<Acc/binary, D/binary>>;
-%% merge(D, <<>>, Acc) ->
-%%     <<Acc/binary, D/binary>>.
-
-%% rcomb(F2, In, Acc) ->
-%%     rcomb(F2, undefined, In, Acc).
+%%--------------------------------------------------------------------
+%% @doc
+%% Combines a set of datasets with a given combinator function.
+%% this requires the combinator to be associative!
+%%--------------------------------------------------------------------
 
 rcomb(F2, _F3, [A], [B]) ->
     F2(A, B);
